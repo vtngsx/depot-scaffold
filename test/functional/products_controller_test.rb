6fe0e4b4d52e7109ea3bba6a830c67fb10ee3d26
vtngsx <vtngsx@gmx.de>
2012-07-09 18:51:36 +0200

    Iteration B1: Validating!

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,47 +3,53 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+     title: 'Lorem Ipsum',
+     description: 'Wibbles are fun!',
+     image_url: 'lorem.jpg',
+     price: 19.95
+   }
+ end
+
+ test "should get index" do
+  get :index
+  assert_response :success
+  assert_not_nil assigns(:products)
+end
+
+test "should get new" do
+  get :new
+  assert_response :success
+end
+
+test "should create product" do
+  assert_difference('Product.count') do
+    post :create, product: @update
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:products)
+  assert_redirected_to product_path(assigns(:product))
+end
+
+test "should show product" do
+  get :show, id: @product
+  assert_response :success
+end
+
+test "should get edit" do
+  get :edit, id: @product
+  assert_response :success
+end
+
+test "should update product" do
+  put :update, id: @product, product: @update
+  assert_redirected_to product_path(assigns(:product))
+end
+
+test "should destroy product" do
+  assert_difference('Product.count', -1) do
+    delete :destroy, id: @product
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create product" do
-    assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
-    end
-
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should show product" do
-    get :show, id: @product
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @product
-    assert_response :success
-  end
-
-  test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete :destroy, id: @product
-    end
-
-    assert_redirected_to products_path
-  end
+  assert_redirected_to products_path
+end
 end
